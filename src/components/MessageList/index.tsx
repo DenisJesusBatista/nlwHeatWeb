dc05bdfeb7bc9a862e554b7a26a817f72a65e856
Denis Batista <denis.si2011@gmail.com>
2021-11-22 17:31:27 -0300

    MessageList

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,14 @@
+import { VscGithubInverted } from 'react-icons/vsc'
+import styles from './styles.module.scss';
+
 export function MessageList() {
     return (
-        <h1>MessageList</h1>
+        <div className={styles.messageListWrapper}>
+            <strong>Entre e compartilhe sua mensagem</strong>
+            <a href="#" className={styles.signInWithGithub}>
+                <VscGithubInverted size="24" />
+                Entrar com GitHub
+            </a>
+        </div>
     )
 }
