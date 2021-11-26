40b66278c313e1512a008696c386eaddcd310ecc
Denis Batista <denis.si2011@gmail.com>
2021-11-26 12:39:30 -0300

    Exibindo as mensagens do prisma-backend no frontend.

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,13 +1,27 @@
 import { api } from '../../services/api';
 import styles from './styles.module.scss';
 import logoImg from '../../assets/logo.svg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export function MessageList() {
+
+    type Message = {
+        id: string;
+        text: string
+        user: {
+            name: string;
+            avatar_url: string;
+        }
+    }
+
+    const [messages, setMessages] = useState<Message[]>([])
+
+
+
     useEffect(() => {
         //Chamada para API
-        api.get('messages/last3').then(response => {
-            console.log(response.data);
+        api.get<Message[]>('messages/last3').then(response => {
+            setMessages(response.data)
         });
     }, [])
     return (
@@ -15,37 +29,19 @@ export function MessageList() {
             <img src={logoImg} alt="DoWhile 2021" />
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/DenisJesusBatista.png" alt="Denis Jesus" />
-                        </div>
-                        <span>Dênis Jesus</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/DenisJesusBatista.png" alt="Denis Jesus" />
-                        </div>
-                        <span>Dênis Jesus</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/DenisJesusBatista.png" alt="Denis Jesus" />
-                        </div>
-                        <span>Dênis Jesus</span>
-                    </div>
-                </li>
-
+                {messages.map(message => {
+                    return (
+                        <li className={styles.message}>
+                            <p className={styles.messageContent}>{message.text}</p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImage}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name}</span>
+                            </div>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     )
