db50b54ef73fb334f04328e9cee92dea2fecc0b5
Denis Batista <denis.si2011@gmail.com>
2021-12-03 09:14:59 -0300

    Conectando usuario com loginGitHub.

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,11 +1,31 @@
+import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc'
 import styles from './styles.module.scss';
 
 export function LoginBox() {
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=e633af659fbe34873e8a`
+
+
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGithubCode = url.includes('?code=');
+
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code=')
+
+            window.history.pushState({}, '', urlWithoutCode);
+
+            // console.log({ urlWithoutCode, githubCode })
+        }
+    }, [])
+
+
+
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
+            <a href={signInUrl} className={styles.signInWithGithub}>
                 <VscGithubInverted size="24" />
                 Entrar com GitHub
             </a>
