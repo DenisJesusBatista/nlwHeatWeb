f703ae0e0df951c476579ff5bd9056ecc82dc74b
Denis Batista <denis.si2011@gmail.com>
2021-12-06 19:10:37 -0300

    Autenticando com Token.

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,9 +1,34 @@
 import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc'
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
+
+type AuthResponse = {
+    token: string;
+    user: {
+        id: string;
+        avatar_url: string;
+        name: string;
+        login: string;
+    }
+}
 
 export function LoginBox() {
     const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=e633af659fbe34873e8a`
+
+    async function signIn(githubCode: string) {
+        const response = await api.post<AuthResponse>('authenticate', {
+            code: githubCode,
+        })
+
+        const { token, user } = response.data;
+
+        localStorage.setItem('@dowhile:token', token);
+
+        console.log(user);
+
+
+    }
 
 
     useEffect(() => {
@@ -16,7 +41,7 @@ export function LoginBox() {
 
             window.history.pushState({}, '', urlWithoutCode);
 
-            // console.log({ urlWithoutCode, githubCode })
+            signIn(githubCode);
         }
     }, [])
 
